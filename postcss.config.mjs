3576aecb77a8ec8d0003b9ae0a823d2155483f72
infoamaanali <infoamaanali@gmail.com>
2025-06-16 11:49:07 +0530

    Fixed Content

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,12 +1,4 @@
-<<<<<<< HEAD
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
   plugins: ["@tailwindcss/postcss"],
 }
-=======
-const config = {
-  plugins: ["@tailwindcss/postcss"],
-};
-
-export default config;
->>>>>>> f0b15a33615f414c81c90dda6d438d0cd457b5a5
